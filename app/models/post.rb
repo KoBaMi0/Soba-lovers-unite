4b891ce52929485a61411841ac5d0682e4e8201f
yusuke <kuromiku77@gmail.com>
2025-06-28 04:23:34 +0900

    fix: posts/newにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
     belongs_to :user
     mount_uploader :avatar, PostImageUploader
+
+    validates :avatar, presence: true
+    validates :title, presence: true, length: { maximum: 255 }
+    validates :body, presence: true, length: { maximum: 65535 }
 end
