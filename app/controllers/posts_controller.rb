b9798067031c012a4c2842821bbed2acde1e372a
yusuke <kuromiku77@gmail.com>
2025-06-28 03:21:27 +0900

    add: 投稿フォームの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
