95e8981bade97956c5f17d4eb20e9a1c5e0a7ef9
miku <kuromiku77@gmail.com>
2025-07-01 02:45:52 +0900

    コメント一覧機能と削除機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
+    before_action :set_reloading_data
+
     def create
-        @comment = Comment.new()
-        @post = Post.find(params[:post_id])
         save_comment = @post.comments.build(comment_params)
 
         if save_comment.save
@@ -11,9 +11,25 @@ class CommentsController < ApplicationController
         end
     end
 
+    def destroy
+        comment_destroy = Comment.find(params[:id])
+
+        if comment_destroy.destroy
+            render "posts/show", status: :created
+        else
+            render "posts/show", status: :bad_request
+        end
+    end
+
     private
 
     def comment_params
         params.require(:comment).permit(:body).merge(user_id: current_user.id, post_id: params[:post_id])
     end
+
+    def set_reloading_data
+        @comment = Comment.new()
+        @post = Post.find(params[:post_id])
+        @comments = @post.comments.includes(:user)
+  end
 end
